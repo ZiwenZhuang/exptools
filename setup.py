288861048a63c09f4e1a6473ef356d5e3aa72d5d
ZiwenZhuang <leo416850130@outlook.com>
2019-12-18 21:06:09 +0800

    version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name= "exptools",
-    version= "0.1.0dev",
+    version= "0.1",
     packages= find_packages(),
     install_requires= [
         "numpy",
