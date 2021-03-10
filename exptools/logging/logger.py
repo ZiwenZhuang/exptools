8ffcbf4f3f1a75c02ffef9f82ff28cc1bc791f69
ZiwenZhuang <leo416850130@outlook.com>
2021-03-10 14:25:38 +0800

    add image storing interface

--- exptools/logging/logger.py
+++ exptools/logging/logger.py
@@ -9,6 +9,7 @@ import os
 import os.path as osp
 import sys
 import datetime
+import imageio
 # import dateutil.tz
 import csv
 # import joblib
@@ -156,7 +157,7 @@ def get_snapshot_mode():
     return _snapshot_mode
 
 
-def set_snapshot_mode(mode):
+def set _snapshot_mode(mode):
     if isinstance(mode, int):
         gap = mode
         mode = "gap"
@@ -189,6 +190,21 @@ def set_disable_prefix(disable_prefix):
 def get_disable_prefix():
     return _disable_prefix
 
+
+def record_image(name, data, itr= None):
+    """ NOTE: data must be (H, W) or (H, W, 3) or (H, W, 4)
+    """
+    os.system("mkdkr -p %s" % os.path.join(_snapshot_dir, "image"))
+    filename = os.path.join(_snapshot_dir, "image", "{}-{}".format(name, itr))
+    imageio.imwrite(filename, data, format= "PNG")
+
+def record_gifs(name, data, itr= None, duration= 0.1):
+    """ record a series of image as gif into file
+    NOTE: data must be a sequence of nparray (H, W) or (H, W, 3) or (H, W, 4)
+    """
+    os.system("mkdkr -p %s" % os.path.join(_snapshot_dir, "gif"))
+    filename = os.path.join(_snapshot_dir, "gif", "{}-{}".format(name, itr))
+    imageio.imwrite(filename, data, format= "GIF", duration= duration)
 
 def log(s, with_prefix=True, with_timestamp=True, color=None):
     if not _disabled:
